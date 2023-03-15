3da5d16712fb43e8b03e640c49a1cf0fe2e5f15c
prajwalraju <prajwalrajup@gmail.com>
2023-03-16 02:57:42 +0530

    added external log file

--- loggingConfig.py
+++ loggingConfig.py
@@ -1,4 +1,4 @@
 import logging
 
 def configure_logging():
-    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+    logging.basicConfig(filename = "Logger.log", format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
