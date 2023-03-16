843fbda1e747de3ef21688b1a68917c93346c3cd
prajwalraju <prajwalrajup@gmail.com>
2023-03-16 14:15:46 +0530

    fixed the Looger file location

--- loggingConfig.py
+++ loggingConfig.py
@@ -1,4 +1,7 @@
+import os
 import logging
 
+
 def configure_logging():
-    logging.basicConfig(filename = "Logger.log", format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+    logging.basicConfig(filename=os.path.dirname(os.path.abspath(__file__)) + "/Logger.log",
+                        format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
